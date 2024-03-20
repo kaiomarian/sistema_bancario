77ae66afdc5d6643c52cd3cd79a4e7bca3ec7345
PEDRO_IGORGAMESBR12 <55291388+KirbyBucciarati@users.noreply.github.com>
2024-03-20 14:25:37 -0300

    Delete cliente.py

--- cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    def __init__ (self, nome, cpf, contas):
-        self.nome = nome
-        self.cpf = cpf
-        self.contas = contas
-        
-    def registrar_conta (self):
-        return self.contas.append(self.contas)
